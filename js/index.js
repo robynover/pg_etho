fdb36a551e96eb5476347af306ad43a677f40d0f
Robyn Overstreet <robynover@gmail.com>
2014-01-21 20:34:25 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,137 @@
+//BROWSER_DEV = true; //quick switching for development in browser
+var app = {
+    // Application Constructor
+    initialize: function() {
+        //console.log('init');
+        this.bindEvents(); // bind events
+    },
+    bindEvents: function() {
+        // the equiv of documentready in jquery. wait for load before proceeding
+        //if (BROWSER_DEV){
+        if (navigator.userAgent.match(/(Mozilla)/)){ //in browser, we'll assume (for debugging)
+            $(document).ready(this.onDeviceReady());   
+            //test connectivity
+            //$.ajax("https://ethoinfo.cloudant.com/_all_dbs").done(function(resp) { console.log(resp); });
+            //$.ajax({url:"https://ethoinfo.cloudant.com/_all_dbs", xhrFields:{withCredentials:true}}).done(function(resp) { console.log(resp); });
+           // alert(navigator.userAgent);        
+        } else {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        }
+        
+        /*if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+            document.addEventListener("deviceready", this.onDeviceReady, false); //this is phonegap
+        } else {
+            this.onDeviceReady(); //this is the browser
+        }*/
+        
+    },
+    onDeviceReady: function(){
+        //alert('loaded');
+        //console.log('ready');
+        
+        //set up handlebars helper
+        Handlebars.registerHelper('formattedDate', function(timestamp) {
+            return moment.utc(timestamp/1000, 'X').zone("-05:00").format('MMMM Do YYYY, H:mm:ss');
+        });
+        //get records from server
+        //pouchSync.sync(FROM_CLOUD);
+        
+        //show existing records
+        app.showRecords();
+
+        // set listener for form button
+        $( "#add_btn" ).on("click touchstart",function() {
+            app.onSubmit();
+            return false;
+         });
+        $( "#form1" ).submit(function() {
+            app.onSubmit();
+            return false;
+         });
+        //sync button
+        $('#sync_cloud').on("click touchstart",function(){
+            console.log('sync fired');
+            pouchSync.sync(TO_CLOUD);
+
+        });
+        /*$('#sync_cloud').on("touchstart",function(){
+            console.log('sync fired');
+            
+            
+        });*/
+        /*$('#testlocalhost').on("click touchstart",function(){
+            console.log('clicked');
+            $('#debug').html('clicked'); 
+            // the url ttp://192.168.1.3/ works from the phone w proper hosts file conf
+            $.ajax("http://192.168.1.3/").done(function(resp) {
+               console.log(resp);
+               $('#debug').html(resp); 
+            });      
+        });*/
+
+    },
+    onSubmit: function(){
+        app.setInputTime($('#utc_time'));
+        if ($('#oname').val().trim().length > 0){
+            app.storeInput($('#utc_time').val(),$('#oname').val().trim());
+            // clear out the field for the next input
+            $('#oname').val('');
+        } else {
+            alert("Please enter a name");
+        }
+        return false;
+    },
+    setInputTime: function(el){
+        if(el){
+            utc = Date.now(); //current time
+            el.val(utc); 
+        }
+        
+    },
+    storeInput:function(dt,oname){
+        // using pouchDB
+        newrecord = {'date':dt,'obs_name':oname};
+        pouchSync.add(newrecord);
+        //append to <ul> list of records in real time
+        this.appendToRecordLi($( "#record_list" ),oname,dt);
+
+    },
+    showRecords: function(){
+
+        pouchSync.all(function(results){
+            //console.log(results);
+            var template = Handlebars.compile($("#record-list-tpl").html());  
+            $('#record_list').html(template(results)); //give it the data
+        });
+    },
+    appendToRecordLi: function(ul_el,name,date){
+        // make the data into an object, formatted the same as an obj with multiple records
+        //   so it can use the same template
+        single_record = [{doc:{'date':date,'obs_name':name}}]; 
+        // have to compile again 
+        var template = Handlebars.compile($("#record-list-tpl").html());  
+        ul_el.append(template(single_record)); //give it the data
+
+    }, 
+    sendToServer: function(){
+        /*
+        Send all the stuff from local storage over to the server
+        */
+       
+        $.ajax({
+          type: "POST",
+          url: "http://phonegap.local:8888/php/receive.php",
+          data: {json: JSON.stringify(this.things)}
+        })
+        .done(function( msg ) {
+            //alert('msg: '+ msg);
+            //console.log('done');
+            $('#debug pre').html(msg);
+          });
+    }
+};
+app.initialize();
+
+
+
+
---
+++ js/pouchSync.js
@@ -0,0 +1,57 @@
+// sync local storage with remote couchDB
+var TO_CLOUD = 1;
+var FROM_CLOUD = 2;
+var BOTH = 3;
+var pouchSync = {
+
+	//remoteCouch: 'http://ethoinfo.iriscouch.com/ethorecords',
+	//cloudant url: https://USERNAME:PASSWORD@USERNAME.cloudant.com/DATABASE 
+	remoteCouch: 'https://ethoinfo:4Z0oSqk7@ethoinfo.cloudant.com/ethorecords', //cloudant won't work bc cross-origin policy
+	db: PouchDB('ethorecords'),
+
+	add: function(record){
+		// take existing record object 
+		// with post, pouch will add _id if there isn't one (using put requires _id value)
+		
+		this.db.post(record, function callback(err, result) {
+			if (!err) {
+				console.log('Successfully added a record!');
+			} else {
+				console.log('could not add record');
+			}
+		});
+	},
+	all: function(callback){ //takes a callback function in order to return records
+		this.db.allDocs({include_docs: true},  //descending: true
+			function (err, doc) {
+		    	callback(doc.rows);
+			});
+	},
+	sync: function(dir){ 
+		/* direction options: TO_CLOUD, FROM_CLOUD, BOTH*/
+		if (!dir){dir = TO_CLOUD;}
+		var opts = {continuous: false,complete:function(){alert('Sync complete');}};
+		if (dir == TO_CLOUD || dir == BOTH){
+			this.db.replicate.to(this.remoteCouch, opts);
+		}
+		if (dir == FROM_CLOUD || dir == BOTH){
+			this.db.replicate.from(this.remoteCouch, opts);
+		}
+		
+	}
+};
+
+
+//newthing = {'date':123,'obs_name':'Robyn'}; //new record
+//pouchSync.add(newthing);
+//pouchSync.sync();
+/*pouchSync.all(function(returnval){
+	console.log(returnval);
+});*/
+
+//console.log(pouchSync.rows);
+
+/*pouchSync.db.allDocs({include_docs: true, descending: true},myCallback());
+function myCallback(err,doc){
+	//console.log('calling back');
+}*/
